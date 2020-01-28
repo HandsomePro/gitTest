1066e4272b0759a2eb2b0ef98c9d221a7b32c4a8
zhangsan <zhangsan@163.com>
2020-01-28 22:15:54 +0800

    把工程添加到版本库

---
+++ RepoGitTest/src/main/java/com/git/repo/RepoTest.java
@@ -0,0 +1,11 @@
+package com.git.repo;
+
+public class RepoTest {
+	public static void main(String[] args) {
+		System.out.println("This is not a drill!");
+		System.out.println("This is not a drill!");
+		System.out.println("This is not a drill!");
+		System.out.println("This is not a drill!");
+		System.out.println("This is not a drill!");
+	}
+}
